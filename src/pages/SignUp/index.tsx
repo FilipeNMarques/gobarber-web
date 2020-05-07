204abcf86c3b260ef2619e84bef99215e5154e50
Filipe Marques <filipi.marques@gmail.com>
2020-05-07 11:52:32 -0300

    using unform for handling forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="Logo GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
-          <Input name="user" icon={FiUser} type="text" placeholder="Nome" />
-          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
           <Input
             name="password"
             icon={FiLock}
             type="password"
             placeholder="Senha"
           />
+
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="create">
           <FiArrowLeft size={20} />
           Voltar para logon
         </a>
       </Content>
     </Container>
-  </>
-);
+  );
+};
 
 export default SignUp;
