b781f3f9f9377f78c85b15a952617289731939ae
Filipe Marques <filipen@rpc.com.br>
2020-07-31 23:43:18 -0300

    feat: estilos de proximo atendimento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
-  console.log(user.avatar_url);
+
   return (
     <>
       <Container>
@@ -27,6 +36,29 @@ const Dashboard: React.FC = () => {
             </button>
           </HeaderContent>
         </Header>
+        <Content>
+          <Schedule>
+            <h1>Horários agendados</h1>
+            <p>
+              <span>Hoje</span>
+              <span>Dia 05</span>
+              <span>Segunda feira</span>
+            </p>
+            <NextAppointment>
+              <strong>Atendimento a seguir</strong>
+              <div>
+                <img src={user.avatar_url} alt={user.name} />
+                <strong>{user.name}</strong>
+
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+              </div>
+            </NextAppointment>
+          </Schedule>
+          <Calendar />
+        </Content>
       </Container>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
 export const Header = styled.header`
   padding: 32px 0;
   background: #28262e;
@@ -47,4 +48,94 @@ export const Profile = styled.div`
       color: #ff9000;
     }
   }
+`;
+
+export const Content = styled.main`
+  display: flex;
+  max-width: 1120px;
+  margin: 64px auto;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+  p {
+    color: #ff9000;
+    margin-top: 8px;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+  span + span::before {
+    content: '';
+    width: 1px;
+    height: 12px;
+    background: #ff9000;
+    margin: 0 8px;
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    color: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 80%;
+      width: 1px;
+      left: 0;
+      top: 10%;
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+
+      svg {
+        color: #ff9000;
+        margin-right: 10px;
+      }
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
 `;
